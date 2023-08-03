0095dee33921f5c5aaa7aaf7aa31492843eebe62
Ana Luiza Ferreira <analuizaffsantos@gmail.com>
2023-08-03 15:11:30 -0300

    segundo commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function cursos(){
+    document.getElementById('msg').innerHTML='Saiba mais';
+    msg.style.textAlign = "center";
+    document.getElementById("trocarimg").src="cursosgratuitos.png";
+}
+
+function nossos(){
+    document.getElementById('msg').innerHTML='Saiba mais';
+    msg.style.textAlign = "center";
+    document.getElementById("trocarimg").src="metodos.png";
+}
+
+function diferenciais(){
+    document.getElementById('msg').innerHTML='Saiba mais';
+    msg.style.textAlign = "center";
+    document.getElementById("trocarimg").src="diferenciais.png";
+}
